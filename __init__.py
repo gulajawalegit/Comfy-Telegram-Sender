38e57265023bb8a556b85dcf2fdca024cb76bb38
gulajawalegit <jokam354@gmail.com>
2025-12-23 00:28:36 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .telegram_sender import TelegramSendVideo
+
+NODE_CLASS_MAPPINGS = {
+    "TelegramSendVideo": TelegramSendVideo
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TelegramSendVideo": "Send Video to Telegram (With Prompt)"
+}
